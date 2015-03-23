7f8b84622d4384984055a24459aed6cc4b9f66d9
Kenyon Tsai <cytsai@gmail.com>
2015-03-23 13:13:41 -0400

    beta

--- convnet.py
+++ convnet.py
@@ -1,1 +1,46 @@
-../convnet.py
+import numpy as np                                                                              # 01: core library used for all computations in the following
+from skimage.util.shape import view_as_windows as unroll                                        # 02: wrapper to numpy as_strided for rolling window view
+
+def pad(data, parameter):                                                                       # 03: PARAMETER as 8-element list for leading/trailing padding widths in all 4 dimensions of DATA
+
+    if np.count_nonzero(parameter) == 0: return data                                            # 04: early stop if nothing to be padded
+    else: return np.pad(data, zip(parameter[0::2],parameter[1::2]), 'constant')                 # 05: zero padding
+
+def forward(data, net):                                                                         # 06: DATA in format N*C*H*W (N*3*227*227 for REFNET input)
+
+    for l in xrange(len(net)):                                                                  # 07: NET (Python list of layers)
+
+        if net[l]['type'] == 'conv':                                                            # 08: CONVOLUTION or INNER_PRODUCT
+            data = pad(data, [0,0,0,0]+net[l]['pad'])                                           # 09: padding DATA in spatial dimensions (H and W) if needed
+            gnum = data.shape[1] / net[l]['filters'].shape[1]                                   # 10: number of FILTER groups G
+            gdat = gnum * [None]                                                                # 11: initializing empty list for caching results
+            fdat = net[l]['filters'].reshape((gnum,-1)+net[l]['filters'].shape[1:])             # 12: reshaping FILTER (C'*C*h*w -> G*C'/G*C*h*w)
+            for g in xrange(gnum):                                                              # 13: loop for FILTER groups
+                gdat[g] = data.reshape((data.shape[0],gnum,-1) + data.shape[2:])[:,g]           # 14: reshaping DATA (N*C*H*W -> N*G*C/G*H*W) into groups and taking the corresponding group
+                gdat[g] = unroll(gdat[g], (1,)+net[l]['filters'].shape[1:]).squeeze((1,4))      # 15: rolling window view of DATA (N*C*H*W -> N*(H-h+1)*(W-w+1)*C*h*w)
+                gdat[g] = gdat[g][:,::net[l]['stride'][0],::net[l]['stride'][1]]                # 16: striding (before computation)
+                gdat[g] = np.tensordot(gdat[g], fdat[g], axes=([3,4,5],[1,2,3]))                # 17: convolution (or inner product)
+            data = np.concatenate(tuple(gdat), -1).transpose(0,3,1,2)                           # 18: concatenating results from all groups and transposing back into default DATA format
+            data = data + net[l]['biases'].reshape(1,-1,1,1)                                    # 19: adding bias terms via broadcasting
+
+        elif net[l]['type'] == 'relu': data = np.maximum(data, 0)                               # 20: RELU
+
+        elif net[l]['type'] == 'pool':                                                          # 21: POOLING (MAX)
+            data = pad(data, [0,0,0,0]+net[l]['pad'])                                           # 22: padding DATA in spatial dimensions (H and W) if needed
+            data = unroll(data, (1,1,)+tuple(net[l]['pool'])).squeeze((4,5))                    # 23: rolling window view of DATA (N*C*H*W -> N*C*(H-h+1)*(W-w+1)*h*w)
+            data = data[:,:,::net[l]['stride'][0],::net[l]['stride'][1]]                        # 24: striding (before computation)
+            data = np.amax(data.reshape(data.shape[0:4]+(-1,)), -1)                             # 25: max pooling
+
+        elif net[l]['type'] == 'normalize':                                                     # 26: LRN
+            ndat = pad(np.square(data), [0,0]+2*[(net[l]['param'][0]-1)/2]+[0,0,0,0])           # 27: padding squared DATA in spectral dimension (C) to simplify boundary conditions
+            ndat = unroll(ndat, (1,net[l]['param'][0],1,1)).squeeze((4,6,7))                    # 28: rolling window view of DATA (N*(C+2c)*H*W -> N*C*H*W*(2c+1))
+            ndat = (net[l]['param'][1] + ndat.sum(-1)*net[l]['param'][2]) ** net[l]['param'][3] # 29: divisor
+            data = data / ndat                                                                  # 30: element-wise normalization
+
+        elif net[l]['type'] == 'softmax':                                                       # 31: SOFTMAX
+            data = np.exp(data - np.amax(data, axis=1)[:,None,:,:])                             # 32: numerically-safe exponential
+            data = data / data.sum(1)[:,None,:,:]                                               # 33: normalizing into probabilities
+
+        else: raise StandardError('Operation in layer {0} undefined!'.format(str(l)))           # 34: throwing error for undefined layer types
+
+    return data                                                                                 # 35: done
--- demo.py
+++ demo.py
@@ -1,1 +1,81 @@
-../demo.py
+import glob
+import pickle
+import numpy as np
+from scipy.ndimage import imread
+from scipy.misc import imresize
+import convnet
+
+# Specify Dataset
+
+dataset = '/home/kenyon/Research/Datasets/ILSVRC2012_img_val/*.JPEG'
+dataset = sorted(glob.glob(dataset))[0:500]
+
+# Load Network
+
+print 'Loading Network'
+network = pickle.load(open('refnet.pkl','rb'))
+results = []
+
+imsize = np.array([227.0, 227.0])
+border = np.array([ 29.0,  29.0])
+
+# Process Dataset
+
+itlist = range(0, len(dataset), 100)
+
+for i in itlist:
+    print 'Processing Stack ' + str(int(i/100)+1) + '/' + str(len(itlist))
+    imstksz = len(dataset)-i if (i==itlist[-1]) else 100
+    imstack = imstksz * [None]
+
+    for j in xrange(imstksz):
+        imstack[j] = imread(dataset[i+j], mode='RGB')[:,:,::-1]
+        imstack[j] = imresize(imstack[j], np.round(max((imsize+border)/imstack[j].shape[0:2])*np.array(imstack[j].shape[0:2])).astype(int))
+
+        croploc = np.floor((np.array(imstack[j].shape[0:2])-imsize) / 2)
+        imstack[j] = imstack[j][croploc[0]:croploc[0]+imsize[0], croploc[1]:croploc[1]+imsize[1]]
+        imstack[j] = imstack[j].transpose(2,0,1)[None,:,:,:]
+        imstack[j] = imstack[j].astype(np.float32) - network['normalization'].astype(np.float32)
+
+    imstack = np.concatenate(tuple(imstack), 0)
+    ftstack = convnet.forward(imstack, network['layers']).squeeze()
+
+    results.append(np.concatenate((np.argsort(ftstack)[:,-1:-6:-1], np.sort(ftstack)[:,-1:-6:-1]), 1))
+
+results = np.concatenate(tuple(results), 0)
+
+# Generate Results in HTML
+
+outfile = open('results.html', 'w')
+outline = '<td align="center"><img data-original="file://{0}" height="100" width="100" class="img-rounded"></td><td>{1}</td>\n'
+
+outfile.write('<!DOCTYPE html>\n\
+               <html lang="en">\n\
+               <head>\n\
+               <title>Results</title>\n\
+               <meta charset="utf-8">\n\
+               <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">\n\
+               <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>\n\
+               <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.lazyload/1.9.1/jquery.lazyload.min.js"></script>\n\
+               <script>$(function() {$("img.img-rounded").lazyload();});</script>\n\
+               </head>\n\
+               <body>\n\
+               <div class="container-fluid">\n\
+               <table class="table table-condensed table-bordered">\n')
+
+for i in xrange(results.shape[0]):
+
+    outentry = ''
+    for j in xrange(5):
+        outentry = outentry + str(int(results[i,j])).zfill(3)+': '
+        outentry = outentry + network['classes'][results[i,j]] + ' '
+        outentry = outentry + '(<i>P</i> = ' + '{:6f}'.format(results[i,j+5]) + ')<br>'
+
+    if (i%5)==0: outfile.write('<tr>\n')
+    outfile.write(outline.format(dataset[i], outentry))
+    if (i%5)==4 or i==(results.shape[0]-1): outfile.write('</tr>\n')
+
+outfile.write('</table> \n\
+               </div> \n\
+               </body> \n\
+               </html>')
